a2b072c75062cfe2efc0c076b97ce0929be62637
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-20 21:08:38 +0900

    Initial commit

---
+++ src/main/java/com/nugu/greenery/NuguServer/NuguServerApplication.java
@@ -0,0 +1,13 @@
+package com.nugu.greenery.NuguServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuguServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuguServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nugu/greenery/NuguServer/NuguServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nugu.greenery.NuguServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuguServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
